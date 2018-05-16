87c9ad466fdbeecf1075bbac49936db5e74e674d
Steven Contreras <steven@terranodo.io>
2018-05-16 08:45:09 -0700

    Update script to reflect correct source paths

--- android/TMControllerSvcLib/TMControllerSvc/src/main/tegola-bin-buildscripts/linux/rebuild_tegola_android_all.sh
+++ android/TMControllerSvcLib/TMControllerSvc/src/main/tegola-bin-buildscripts/linux/rebuild_tegola_android_all.sh
@@ -19,7 +19,7 @@ t_platform=(android-arm android-arm64 android-x86 android-x86_64)
 
 for index in ${!t_platform[*]} ; do
     echo rebuild_tegola_android_all.sh: Running tegola android-platform build: ${t_platform[$index]}
-    eval "${MY_ANDROID_STUDIO_WORKSPACE}/src/github.com/terranodo/tegola-mobile/android/ControllerLib/Controller/src/main/tegola-bin-buildscripts/linux/clean_tegola.sh -t_platform ${t_platform[$index]} $REQUIRED_ARGS"
-    eval "${MY_ANDROID_STUDIO_WORKSPACE}/src/github.com/terranodo/tegola-mobile/android/ControllerLib/Controller/src/main/tegola-bin-buildscripts/linux/build_tegola.sh -t_platform ${t_platform[$index]} $COMMON_ARGS"
+    eval "${MY_ANDROID_STUDIO_WORKSPACE}/src/github.com/terranodo/tegola-mobile/android/TMControllerSvcLib/TMControllerSvc/src/main/tegola-bin-buildscripts/linux/clean_tegola.sh -t_platform ${t_platform[$index]} $REQUIRED_ARGS"
+    eval "${MY_ANDROID_STUDIO_WORKSPACE}/src/github.com/terranodo/tegola-mobile/android/TMControllerSvcLib/TMControllerSvc/src/main/tegola-bin-buildscripts/linux/build_tegola.sh -t_platform ${t_platform[$index]} $COMMON_ARGS"
     echo
 done
